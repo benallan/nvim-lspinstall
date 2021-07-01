a5fd61984ed19b36923c5a68b1c50760e82d5fad
Ben Allan <benallan@fastmail.com>
2021-07-01 20:32:00 +0900

    Windows install script for efm

--- lua/lspinstall/servers/efm.lua
+++ lua/lspinstall/servers/efm.lua
@@ -1,9 +1,25 @@
 local config = require"lspinstall/util".extract_config("efm")
-config.default_config.cmd[1] = "./efm-langserver"
 
-return vim.tbl_extend('error', config, {
+local install_script
+
+if require"lspinstall/util".detect_windows() then
+
+  config.default_config.cmd[1] = "./efm-langserver.exe"
+
+  install_script = [[
+  $Env:GOPATH = $pwd; $Env:GOBIN = $pwd; $Env:GO111MODULE = "on";
+  go get -v github.com/mattn/efm-langserver
+  go clean -modcache
+  ]]
+
+else
+
+  config.default_config.cmd[1] = "./efm-langserver"
+
   install_script = [[
   GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v github.com/mattn/efm-langserver
   GOPATH=$(pwd) GO111MODULE=on go clean -modcache
   ]]
-})
+end
+
+return vim.tbl_extend('error', config, { install_script = install_script })
