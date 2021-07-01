d8a9b90f7d3829c7f4b73def2d6821d3a2e4a656
Ben Allan <benallan@fastmail.com>
2021-07-01 20:20:00 +0900

    Windows install script for bash

--- lua/lspinstall/servers/bash.lua
+++ lua/lspinstall/servers/bash.lua
@@ -1,9 +1,25 @@
 local config = require"lspinstall/util".extract_config("bashls")
-config.default_config.cmd[1] = "./node_modules/.bin/bash-language-server"
 
-return vim.tbl_extend('error', config, {
+local install_script
+
+if require"lspinstall/util".detect_windows() then
+
+  config.default_config.cmd[1] = "./node_modules/.bin/bash-language-server.cmd"
+
+  install_script = [[
+    if (-Not (Test-Path -Path .\package.json -PathType Leaf)){ npm init -y --scope=lspinstall }
+    npm install bash-language-server@latest
+  ]]
+
+else
+
+  config.default_config.cmd[1] = "./node_modules/.bin/bash-language-server"
+
   install_script = [[
   ! test -f package.json && npm init -y --scope=lspinstall || true
   npm install bash-language-server@latest
   ]]
-})
+
+end
+
+return vim.tbl_extend('error', config, { install_script = install_script })
